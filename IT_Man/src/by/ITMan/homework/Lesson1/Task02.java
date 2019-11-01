2925611329b60955c82a352ab105543ec3c7cfb5
wait-tomorrow <54080773+wait-tomorrow@users.noreply.github.com>
2019-11-01 11:49:30 +0300

    Add files via upload

---
+++ IT_Man/src/by/ITMan/homework/Lesson1/Task02.java
@@ -0,0 +1,29 @@
+package by.ITMan.homework.Lesson1;
+
+/*  2) Task2 Вводиться год (целое число: как положительное, так и отрицательное - до нашей эры)
+    Программа должна вывести век и AD для нашей эры и BC до нашей эры
+
+    Примеры:
+    2019
+    21AD
+
+    -29
+    1BC
+*/
+
+public class Task02 {
+    public static void main(String[] args) {
+        int year = Integer.parseInt(args[0]);
+        int century = year / 100;
+
+        if (year % 100 != 0) {
+            century++;
+        }
+
+        if (year > 0) {
+            System.out.println(century + "AD");
+        } else {
+            System.out.println(century + "BC");
+        }
+    }
+}
