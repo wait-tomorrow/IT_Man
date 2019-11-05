4ddb386034df691edf9fad2e03dd469fd11e749d
Dmitry Wroblewski <dima.vrb@gmail.com>
2019-11-05 15:16:13 +0300

    Task 2 is done

--- src/by/ITMan/homework/Lesson2/Task02.java
+++ src/by/ITMan/homework/Lesson2/Task02.java
@@ -3,13 +3,9 @@ package by.ITMan.homework.Lesson2;
 import java.util.Random;
 
 public class Task02 {
-    public static void main(String[] args) {
-        int[][] matrix = createMatrix(3);
 
-        printMatrix(matrix);
-        System.out.println(calculateDiagsDifferece(matrix));
-    }
-
+    // CPU      O(n)
+    // Memory:  O(1)
     public static int calculateDiagsDifferece(int[][] matrix) {
         int diagSum1 = 0;
         int diagSum2 = 0;
@@ -17,31 +13,9 @@ public class Task02 {
 
         for (int i = 0; i < matrixSize; i++) {
             diagSum1 = diagSum1 + matrix[i][i];
-            diagSum2 = diagSum2 + matrix[i][matrixSize-i-1];
+            diagSum2 = diagSum2 + matrix[i][matrixSize - i - 1];
         }
 
         return diagSum1 - diagSum2;
-    }
-
-    public static int[][] createMatrix(int size) {
-        int[][] matrix = new int[size][size];
-        Random r = new Random(System.currentTimeMillis());
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = r.nextInt(20) - 10;
-            }
-        }
-
-        return matrix;
-    }
-
-    public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.printf("%5d", matrix[i][j]);
-            }
-            System.out.println();
-        }
     }
 }
