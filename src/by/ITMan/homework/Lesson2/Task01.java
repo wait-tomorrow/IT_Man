62f86b3a6c0d40dfe914171f89056eadab93056e
Dmitry Wroblewski <dima.vrb@gmail.com>
2019-11-05 22:29:28 +0300

    minor fix 2

--- src/by/ITMan/homework/Lesson2/Task01.java
+++ src/by/ITMan/homework/Lesson2/Task01.java
@@ -6,17 +6,13 @@ public class Task01 {
     // Memory:  O(1)
     public static int findSecondMinimalElem(int[] a) {
         int minFirst = Integer.MAX_VALUE;
-        int minFirstInd = -1;
         int minSecond = Integer.MAX_VALUE;
 
         for (int i = 0; i < a.length; i++) {
             if (a[i] < minFirst) {
                 minSecond = minFirst;
                 minFirst = a[i];
-                minFirstInd = i;
-            }
-
-            if (a[i] <= minSecond && i != minFirstInd) {
+            } else if (a[i] <= minSecond) {
                 minSecond = a[i];
             }
         }
