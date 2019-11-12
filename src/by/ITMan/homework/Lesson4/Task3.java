c23f32b6544c7dad2b827f028a5ee988648d92bc
Dmitry Wroblewski <dima.vrb@gmail.com>
2019-11-12 22:00:08 +0300

    Task3 second attempt

--- src/by/ITMan/homework/Lesson4/Task3.java
+++ src/by/ITMan/homework/Lesson4/Task3.java
@@ -1,11 +1,36 @@
 package by.ITMan.homework.Lesson4;
 
+import java.util.Scanner;
+
 public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] errors = new int[n];
+        int[] errorsAfterFirstFix = new int[n - 1];
+        int[] errorsAfterSecondFix = new int[n - 2];
+
+        for (int i = 0; i < errors.length; i++) {
+            errors[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < errorsAfterFirstFix.length; i++) {
+            errorsAfterFirstFix[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < errorsAfterSecondFix.length; i++) {
+            errorsAfterSecondFix[i] = sc.nextInt();
+        }
+
+        findFixedErrors(n, errors, errorsAfterFirstFix, errorsAfterSecondFix);
+    }
 
     // CPU:     O(n)
     // Memory:  O(n)
     public static long[] findFixedErrors(int errorsCount, int[] errors, int[] errorsAfterFirstFix, int[] errorsAfterSecondFix) {
-        long fixedErrors[] = new long[2];
+        long[] fixedErrors = new long[2];
         long errorsSum = 0;
         long errors1 = 0;
         long errors2 = 0;
@@ -14,12 +39,12 @@ public class Task3 {
             errorsSum = errorsSum + (long) errors[i];
         }
 
-        for (int i = 0; i < errorsAfterFirstFix.length; i++) {
-            errors1 = errors1 + (long) errorsAfterFirstFix[i];
+        for (int afterFirstFix : errorsAfterFirstFix) {
+            errors1 = errors1 + (long) afterFirstFix;
         }
 
-        for (int i = 0; i < errorsAfterSecondFix.length; i++) {
-            errors2 = errors2 + (long) errorsAfterSecondFix[i];
+        for (int afterSecondFix : errorsAfterSecondFix) {
+            errors2 = errors2 + (long) afterSecondFix;
         }
 
         fixedErrors[0] = errorsSum - errors1;
